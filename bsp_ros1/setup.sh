02e5d426bf78088f0d56f2ca14571753aa193514
Daisuke Sato <tiryoh@gmail.com>
2022-11-26 18:39:50 +0900

    update mini_pupper_bsp to the latest

--- bsp_ros1/setup.sh
+++ bsp_ros1/setup.sh
@@ -33,9 +33,7 @@ fi
 $BASEDIR/configure_network.sh $1 "$2"
 
 cd ~
-#TODO change after PR is merged
-#git clone https://github.com/mangdangroboticsclub/mini_pupper_bsp.git
-git clone https://github.com/hdumcke/mini_pupper_bsp.git -b PR5
+git clone https://github.com/mangdangroboticsclub/mini_pupper_bsp.git
 git clone https://github.com/Tiryoh/ros_setup_scripts_ubuntu.git
 ./mini_pupper_bsp/install.sh
 ~/ros_setup_scripts_ubuntu/ros-noetic-ros-base-main.sh
